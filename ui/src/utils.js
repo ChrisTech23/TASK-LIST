a5e8b46f70fa06089c05f3cb55829cbce5ccc78a
christech223 <techtechtech223@gmail.com>
2024-01-05 03:00:58 -0500

    feat: update api url

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,2 +1,1 @@
-export const API_URL = 
-"https://mz6cdonco26rmsj2trotfy2k2e0hosvr.lambda-url.us-east-1.on.aws/task";
+export const API_URL = "https://mz6cdonco26rmsj2trotfy2k2e0hosvr.lambda-url.us-east-1.on.aws/task";
